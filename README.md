71c7724e51dc1915435c363d5ded08ca4e92250d
Lucas Alves <luk4z_7@hotmail.com>
2022-07-19 23:59:05 -0300

    Feat: Added the menu to user navigate into them
    feat:	Created a matched options over enums that is created
    	to implements the options by the user, end, start and play again
    
    feat:	Created the methods into State that is responsible to this
    	operations when the user click into options into menu

---
+++ README.md
@@ -0,0 +1,3 @@
+## xGame
+
+xGame built on top of bracket-lib, that control the event loop and window events
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,62 @@
 
 use bracket_lib::prelude::*;
 
-struct State {}
+struct State {
+    mode: GameMode,
+}
+
+impl State {
+    fn new() -> Self {
+        State {
+            mode: GameMode::Menu,
+        }
+    }
+
+    fn play(&mut self, _: &mut BTerm) {
+        self.mode = GameMode::End
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to xGame");
+        ctx.print_centered(10, "(P) Play Game");
+        ctx.print_centered(12, "(Q) Quit Game");
+
+        // Check what type the letter the user choice
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {} // do nothing
+            }
+        }
+    }
+
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing
+    }
+
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "You are dead!");
+        ctx.print_centered(10, "(P) Play Again");
+        ctx.print_centered(12, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {} // do nothing
+            }
+        }
+    }
+}
+
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
 
 // implements a trait the brackets expos
 // This is similar to implementing functions for a structure,
@@ -14,14 +69,22 @@ impl GameState for State {
         // accessing information like mouse position and
         // keyboard input, and sending commands to draw to the window.
         ctx.cls(); // cls clear the window
-        ctx.print(1, 1, "Hello, Bracket Terminal!"); // 1, 1 is the coordiantes
-                                                     // representing where you want the text to appear
+        ctx.print(1, 1, "running..."); // 1, 1 is the coordiantes
+                                       // representing where you want the text to appear
+
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::End => self.dead(ctx),
+            GameMode::Playing => self.play(ctx),
+        }
     }
 }
 
 fn main() -> BError {
     // build? only works because we return a BError
     // that's an Result
-    let context = BTermBuilder::simple80x50().with_title("X-Dragon").build()?;
-    main_loop(context, State {})
+    let context = BTermBuilder::simple80x50().with_title("xGame").build()?;
+
+    // main_loop starts the game loop and begins calling tick function on every frame
+    main_loop(context, State::new())
 }
