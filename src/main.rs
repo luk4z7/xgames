4622a5370a8e1e04244e3820b1b809cc0924b28e
Lucas Alves <luk4z_7@hotmail.com>
2022-07-23 14:50:34 -0300

    Made a player to our game
    feat:	Included a character to simulate the player, using
    	the keyboard space to navigate into the game and
    	jump to not dead into the game
    
    feat:	Created a struct Player and Impl same methods, like constructor
    	gravity_and_move, flap and render
    
    fix:	Changed some methods to made this game and player work together,
    	included the new fields into the Game struct to initiate a new
    	player and the game started

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,86 @@
 
 use bracket_lib::prelude::*;
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+
 struct State {
+    player: Player,
+
+    // This tracks the time accumulated between frames to control the game’s speed.
+    frame_time: f32,
     mode: GameMode, // enum type
+}
+
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        // set is a ‘bracket-lib‘ function that sets a single character on the screen.
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@')); // converts a Unicode symbol from your source code to the matching Codepage 437 character number
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+
+        self.y += self.velocity as i32; // this is a cast to i32 to make the sum with self.y
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
 }
 
 impl State {
     // Constructor that return self
     fn new() -> Self {
         State {
+            player: Player::new(5, 25),
+            frame_time: 0.0,
             mode: GameMode::Menu,
         }
     }
 
-    fn play(&mut self, _: &mut BTerm) {
-        self.mode = GameMode::End
+    fn play(&mut self, ctx: &mut BTerm) {
+        ctx.cls_bg(NAVY); // spefify the background color
+
+        // The tick function runs as fast as it can—often 60 or more times per second.
+        // The context provides a variable named frame_time_ms containing the time
+        // elapsed since the last time tick was called.
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to flap.");
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End;
+        }
     }
 
     // This is the principal functions to welcome
@@ -32,6 +98,8 @@ impl State {
     }
 
     fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing
     }
 
