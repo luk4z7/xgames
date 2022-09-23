b4c0d3706385b7dd3f6d9f77b9f2ae6d8ed717b3
Lucas Alves <luk4z_7@hotmail.com>
2022-09-23 01:42:52 -0300

    refactored the game design
    feat:	Changed the design of game to something more customized

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,115 @@ use bracket_lib::prelude::*;
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 const FRAME_DURATION: f32 = 75.0;
+const DRAGON_FRAMES: [u16; 6] = [64, 1, 2, 3, 2, 1];
+
+struct Obstacle {
+    x: i32,
+    gap_y: i32,
+    size: i32,
+}
+
+impl Obstacle {
+    fn new(x: i32, score: i32) -> Self {
+        let mut random = RandomNumberGenerator::new();
+
+        Obstacle {
+            x,
+            gap_y: random.range(5, 20),
+            size: i32::max(2, 20 - score),
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
+        // The ground
+        for x in 0..SCREEN_WIDTH {
+            ctx.set(x, SCREEN_HEIGHT - 1, WHITE, WHITE, to_cp437('#'));
+        }
+
+        let screen_x = self.x - player_x;
+        let half_size = self.size / 2;
+        // Top wall
+        // Draw the top half of the obstacles
+        for y in 0..self.gap_y - half_size {
+            ctx.set(screen_x, y, WHITE, NAVY, 179);
+        }
+
+        // Bottom wall - now leaving room for the ground
+        // Draw the bottom half of the obstacle
+        for y in self.gap_y + half_size..SCREEN_HEIGHT - 1 {
+            ctx.set(screen_x, y, WHITE, NAVY, 179);
+        }
+    }
+
+    // This function receives a borrowed reference to the player as a parameter
+    fn hit_obstacle(&self, player: &Player) -> bool {
+        let half_size = self.size / 2;
+        player.x == self.x
+            && ((player.y as i32) < self.gap_y - half_size
+                || player.y as i32 > self.gap_y + half_size)
+    }
+}
+
+struct Player {
+    x: i32,
+    y: f32,
+    velocity: f32,
+    frame: usize, // Usize to index arrays
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y: y as f32,
+            velocity: 0.0,
+            frame: 0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        // set is a ‘bracket-lib‘ function that sets a single character on the screen.
+        // ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@')); // converts a Unicode symbol from your source code to the matching Codepage 437 character number
+        ctx.set_active_console(1);
+        ctx.cls();
+        ctx.set_fancy(
+            PointF::new(0.0, self.y),
+            1,
+            Degrees::new(0.0),
+            PointF::new(2.0, 2.0),
+            WHITE,
+            NAVY,
+            DRAGON_FRAMES[self.frame],
+        );
+        ctx.set_active_console(0);
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.1;
+        }
+
+        self.y += self.velocity; // this is a cast to i32 to make the sum with self.y
+                                 // self.x += 1;
+        if self.y < 0.0 {
+            self.y = 0.0;
+        }
+
+        self.x += 1;
+        self.frame += 1;
+        self.frame = self.frame % 6; // % is modulus - remainder
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -1.0;
+    }
+}
+
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
 
 struct State {
     player: Player,
@@ -14,85 +123,6 @@ struct State {
     mode: GameMode, // enum type
     obstacle: Obstacle,
     score: i32,
-}
-
-struct Obstacle {
-    x: i32,
-    gap_y: i32,
-    size: i32,
-}
-
-struct Player {
-    x: i32,
-    y: i32,
-    velocity: f32,
-}
-
-impl Obstacle {
-    fn new(x: i32, score: i32) -> Self {
-        let mut random = RandomNumberGenerator::new();
-
-        Obstacle {
-            x,
-            gap_y: random.range(10, 40),
-            size: i32::max(2, 20 - score),
-        }
-    }
-
-    fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
-        let screen_x = self.x - player_x;
-        let half_size = self.size / 2;
-
-        // Draw the top half of the obstacles
-        for y in 0..self.gap_y - half_size {
-            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'));
-        }
-
-        // Draw the bottom half of the obstacle
-        for y in self.gap_y + half_size..SCREEN_HEIGHT {
-            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'));
-        }
-    }
-
-    // This function receives a borrowed reference to the player as a parameter
-    fn hit_obstracle(&self, player: &Player) -> bool {
-        let half_size = self.size / 2;
-        let does_x_match = player.x == self.x;
-        let player_above_gap = player.y < self.gap_y - half_size;
-        let player_below_gap = player.y > self.gap_y + half_size;
-        does_x_match && (player_above_gap || player_below_gap)
-    }
-}
-
-impl Player {
-    fn new(x: i32, y: i32) -> Self {
-        Player {
-            x,
-            y,
-            velocity: 0.0,
-        }
-    }
-
-    fn render(&mut self, ctx: &mut BTerm) {
-        // set is a ‘bracket-lib‘ function that sets a single character on the screen.
-        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@')); // converts a Unicode symbol from your source code to the matching Codepage 437 character number
-    }
-
-    fn gravity_and_move(&mut self) {
-        if self.velocity < 2.0 {
-            self.velocity += 0.2;
-        }
-
-        self.y += self.velocity as i32; // this is a cast to i32 to make the sum with self.y
-        self.x += 1;
-        if self.y < 0 {
-            self.y = 0;
-        }
-    }
-
-    fn flap(&mut self) {
-        self.velocity = -2.0;
-    }
 }
 
 impl State {
@@ -133,7 +163,7 @@ impl State {
             self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
         }
 
-        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstracle(&self.player) {
+        if self.player.y as i32 > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
             self.mode = GameMode::End;
         }
     }
@@ -144,15 +174,15 @@ impl State {
     // tick fn this is matched and applied
     fn main_menu(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print_centered(5, "Welcome to xGame");
-        ctx.print_centered(10, "(P) Play Game");
-        ctx.print_centered(12, "(Q) Quit Game");
+        ctx.print_color_centered(5, YELLOW, BLACK, "Welcome to Flappy Dragon");
+        ctx.print_color_centered(8, CYAN, BLACK, "(P) Play Game");
+        ctx.print_color_centered(9, CYAN, BLACK, "(Q) Quit Game");
 
         self.apply_click(ctx);
     }
 
     fn restart(&mut self) {
-        self.player = Player::new(5, 25);
+        self.player = Player::new(5, SCREEN_WIDTH / 2);
         self.frame_time = 0.0;
         self.obstacle = Obstacle::new(SCREEN_WIDTH, 0);
         self.score = 0;
@@ -161,11 +191,10 @@ impl State {
 
     fn dead(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print_centered(5, "You are dead!");
+        ctx.print_color_centered(5, RED, BLACK, "You are dead!");
         ctx.print_centered(6, &format!("You earned {} points", self.score));
-
-        ctx.print_centered(10, "(P) Play Again");
-        ctx.print_centered(12, "(Q) Quit Game");
+        ctx.print_color_centered(8, CYAN, BLACK, "(P) Play Again");
+        ctx.print_color_centered(9, CYAN, BLACK, "(Q) Quit Game");
 
         self.apply_click(ctx);
     }
@@ -181,12 +210,6 @@ impl State {
             }
         }
     }
-}
-
-enum GameMode {
-    Menu,
-    Playing,
-    End,
 }
 
 // implements a trait the brackets expos
@@ -214,7 +237,13 @@ impl GameState for State {
 fn main() -> BError {
     // build? only works because we return a BError
     // that's an Result
-    let context = BTermBuilder::simple80x50().with_title("xGame").build()?;
+    let context = BTermBuilder::new()
+        .with_font("../resources/flappy32.png", 32, 32)
+        .with_simple_console(SCREEN_WIDTH, SCREEN_HEIGHT, "../resources/flappy32.png")
+        .with_fancy_console(SCREEN_WIDTH, SCREEN_HEIGHT, "../resources/flappy32.png")
+        .with_title("Flappy Dragon Enhanced")
+        .with_tile_dimensions(16, 16)
+        .build()?;
 
     // main_loop starts the game loop and begins calling tick function on every frame
     main_loop(context, State::new())
